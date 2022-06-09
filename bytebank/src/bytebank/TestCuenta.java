6ed094140f4e61583226569399dd6b395ac68667
diegofdg <diegofdg@gmail.com>
2022-06-09 20:42:23 -0300

    clase_04-herencia_y_el_uso_de_constructores

---
+++ bytebank/src/bytebank/TestCuenta.java
@@ -0,0 +1,14 @@
+package bytebank;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+		CuentaCorriente cc = new CuentaCorriente(1, 111);
+		CuentaAhorro ca = new CuentaAhorro(2, 333);
+		cc.depositar(2000);
+		cc.transferir(1000, ca);
+		
+		System.out.println(cc.getSaldo());
+		System.out.println(ca.getSaldo());
+	}
+}
